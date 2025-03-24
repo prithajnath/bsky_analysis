2ae38c5a3eafa0b2e8366a4c59d1ce7da709efdb
Prithaj Nath <prithajnath@gmail.com>
2025-03-24 11:48:52 -0400

    normalize date range in cte

--- sql/rolling_6_week_avg.sql
+++ sql/rolling_6_week_avg.sql
@@ -1,10 +1,23 @@
-with daily_counts as (
-    select 
+with custom_date_range as (
+    select
+     unnest(generate_series(timestamp '2022-01-01', timestamp '2025-03-01', interval '1 day'))::date as post_date
+),
+user_posts_in_custom_date_range as (
+    select
+        a.author_did,
+        b.post_date::date as post_date,
+        ap.cid,
+    from (select distinct author_did from all_posts) a
+    cross join custom_date_range b
+    left join all_posts ap on ap.created_at::date = b.post_date and ap.author_did = a.author_did
+), daily_counts as (
+    select
         author_did,
-        created_at::date as post_date,
-        count(*) as post_count
-    from all_posts
-    group by 1,2 
+        post_date,
+        coalesce(count(cid), 0) as post_count
+    from user_posts_in_custom_date_range
+    group by 1,2
+    order by 1,2
 ),
 rolling_avg as (
     select 
